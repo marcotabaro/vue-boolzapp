deae5e928f91e3419b6b752fbc904ad38db8e23a
marco <g.marcotaba@gmail.com>
2020-11-23 23:04:21 +0100

    new array for messages

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const message = [
+    {
+        text: 'La marianna va in campagna',
+        sent: true,
+        date: new Date(),
+    }
+];
+
+
 const myApp = new Vue ({
     el: '#root',
     data: {
@@ -5,19 +14,27 @@ const myApp = new Vue ({
         contacts: [
             {
                 name: 'Michele',
-                propic: 'img/avatar_1.jpg'
+                propic: 'img/avatar_1.jpg',
+                message: [...message],
+                lastSeen: ''
             },
             {
                 name: 'Fabio',
-                propic: 'img/avatar_2.jpg'
+                propic: 'img/avatar_2.jpg',
+                message: [...message],
+                lastSeen: ''
             },
             {
                 name: 'Samuele',
-                propic: 'img/avatar_3.jpg'
+                propic: 'img/avatar_3.jpg',
+                message: [...message],
+                lastSeen: ''
             },
             {
                 name: 'Luisa',
-                propic:'img/avatar_4.jpg'
+                propic:'img/avatar_4.jpg',
+                message: [...message],
+                lastSeen: ''
             }
         ],
         index: 0,
