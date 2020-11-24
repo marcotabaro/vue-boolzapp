20a14fa48be5b72a584c48f3a1a05a7f72e0a12a
marco <g.marcotaba@gmail.com>
2020-11-24 14:45:46 +0100

    Implemented logic, working on style

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 
-const message = [
-    {
-        text: [],
-        sent: true,
-        date: new Date(),
-    }
-];
-
+// const message = [
+//     {
+//         text: [],
+//         sent: true,
+//         date: new Date(),
+//     }
+// ];
+let date = new Date();
+let currentDate = date.toLocaleDateString() + ' ' + date.toLocaleTimeString();
 
 const myApp = new Vue ({
     el: '#root',
@@ -16,29 +17,74 @@ const myApp = new Vue ({
             {
                 name: 'Michele',
                 propic: 'img/avatar_1.jpg',
-                message: randomMessage = text[Math.floor(Math.random() * text.length)],
-                lastSeen: ''
+                lastSeen: '',
+                message: [
+                    {
+                        text: quotes[Math.floor(Math.random() * quotes.length)],
+                        sent: false,
+                        date: currentDate,
+                    }
+                ]
             },
             {
                 name: 'Fabio',
                 propic: 'img/avatar_2.jpg',
-                message: randomMessage = text[Math.floor(Math.random() * text.length)],
-                lastSeen: ''
+                lastSeen: '',
+                message: [
+                    {
+                        text:quotes[Math.floor(Math.random() * quotes.length)],
+                        sent: false,
+                        date: currentDate,
+                    }
+                ]
             },
             {
                 name: 'Samuele',
                 propic: 'img/avatar_3.jpg',
-                message: randomMessage = text[Math.floor(Math.random() * text.length)],
-                lastSeen: ''
+                lastSeen: '',
+                message: [
+                    {
+                        text:quotes[Math.floor(Math.random() * quotes.length)],
+                        sent: false,
+                        date: currentDate,
+                    }
+                ]
             },
             {
                 name: 'Luisa',
                 propic:'img/avatar_4.jpg',
-                message: randomMessage = text[Math.floor(Math.random() * text.length)],
-                lastSeen: ''
+                lastSeen: '',
+                message: [
+                    {
+                        text:quotes[Math.floor(Math.random() * quotes.length)],
+                        sent: false,
+                        date: currentDate,
+                    }
+                ]
             }
         ],
-        index: 0,
+        contactIndex: 0,
+        textBox: ''
+    },
+    methods: {
+        contactChange: function (index) {
+            this.contactIndex = index;
+        },
+        send: function () {
+            this.contacts[this.contactIndex].message.push({
+                text: this.textBox,
+                sent: true,
+                date: currentDate
+            })
+            this.textBox = '';
+            setTimeout(this.received, 5000);
+        },
+        received: function () {
+            this.contacts[this.contactIndex].message.push ({
+                text: quotes[Math.floor(Math.random() * quotes.length)],
+                sent: false,
+                date: currentDate
+            })
+        }
     }
 })
-
